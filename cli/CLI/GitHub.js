5f498604ac19c98660f2f0752cc5aafdd790189a
Mark <mark.eibes@gmail.com>
2026-02-11 22:36:37 +0100

    Replace JS CLI with PureScript CLI using Om and yoga-fetch-om

--- cli/CLI/GitHub.js
+++
@@ -1,8 +0,0 @@
-export function fetchTextImpl(url) {
-  return function () {
-    return fetch(url).then(function (r) {
-      if (!r.ok) throw new Error("HTTP " + r.status + ": " + url);
-      return r.text();
-    });
-  };
-}
--- cli/CLI/GitHub.purs
+++ cli/CLI/GitHub.purs
@@ -4,26 +4,34 @@ import Prelude
 
 import CLI.Registry (Registry)
 import Data.Either (Either(..))
-import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Exception (throw) as Exception
+import JS.Fetch as Fetch
+import JS.Fetch.Request as Request
+import JS.Fetch.Response as Response
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync as FS
-import Promise (Promise)
-import Promise.Aff (toAffE)
+import Promise.Aff as Promise
 import Yoga.JSON (readJSON)
 import Yoga.Om (Om, fromAff)
 
 baseUrl :: String
 baseUrl = "https://raw.githubusercontent.com/rowtype-yoga/purescript-yoga-shadcn/main"
 
-foreign import fetchTextImpl :: String -> Effect (Promise String)
+fetchText :: forall ctx err. String -> Om ctx err String
+fetchText url = fromAff do
+  req <- liftEffect $ Request.new url {}
+  resp <- Promise.toAffE (Fetch.fetch req)
+  let status = Response.status resp
+  when (status /= 200) do
+    liftEffect $ Exception.throw ("HTTP " <> show status <> ": " <> url)
+  Promise.toAffE (Response.text resp)
 
 fetchFile :: forall ctx err. String -> Om ctx err String
 fetchFile path = do
   localExists <- liftEffect (FS.exists path)
   if localExists then liftEffect (FS.readTextFile UTF8 path)
-  else toAffE (fetchTextImpl (baseUrl <> "/" <> path)) # fromAff
+  else fetchText (baseUrl <> "/" <> path)
 
 fetchRegistry :: forall ctx err. Om ctx err Registry
 fetchRegistry = do
@@ -35,4 +43,4 @@ fetchRegistry = do
   readRegistryText = do
     localExists <- liftEffect (FS.exists "cli/registry.json")
     if localExists then liftEffect (FS.readTextFile UTF8 "cli/registry.json")
-    else toAffE (fetchTextImpl (baseUrl <> "/cli/registry.json")) # fromAff
+    else fetchText (baseUrl <> "/cli/registry.json")
